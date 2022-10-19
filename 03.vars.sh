dbbedd6486dbcb3ee0f36edec16fcdf5684a0ec4
praveenarupi <praveenarupi@gmail.com>
2022-10-19 13:21:04 +0530

    updating ./learning-shell

---
+++ 03.vars.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+a=10
+echo Value of a = $a
+
+date=2022-08-05
+echo Today date is $date
+
+date1=$(date +%F)
+echo Todays date is $date1
+
+b=$((2+3-5*6))
+echo Value of b = $b
+
+## This is env variable
+echo Value of c = $c
