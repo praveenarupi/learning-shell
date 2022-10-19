b471ef0bbed4f0bbf61a660868acb8c888fd9692
praveenarupi <praveenarupi@gmail.com>
2022-10-19 13:22:08 +0530

    updating ./learning-shell

---
+++ 05.functions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+## Declare a function
+Sample() {
+  a=30
+  echo Sample Function
+  echo a from main program = $a
+  b=20
+}
+
+## Access a function , Simply function name
+a=10
+Sample
+echo b from function = $b
+echo a in main program = $a
